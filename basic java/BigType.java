9b3e68c4bfba8b0720079b405cefc3a66b146bbf
PrinceK93 <princed55x@gmail.com>
2025-08-01 18:47:14 +0530

    Add BigType and DateType classes with main methods for BigInteger and Date operations

---
+++ basic java/BigType.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+
+public class BigType {
+    public static void main(String[] args) {
+        BigInteger b = new BigInteger("191291929192192192219291921");
+        BigInteger b2 = new BigInteger("99999999929192192192219291921");
+        BigInteger b3 = b.add(b2);
+        System.out.println(b3);
+    }
+}
---
+++ basic java/DateType.java
@@ -0,0 +1,16 @@
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateType {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+        // change the date and time and months use Calendar
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.MONTH,10);
+        System.out.println(c.getTime());
+
+    }
+}
