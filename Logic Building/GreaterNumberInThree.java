8993232306b87d11451379a9e584feb17d01e068
PrinceK93 <princed55x@gmail.com>
2025-08-01 18:56:15 +0530

    Add GreaterNumberInThree class with main method to determine the greatest of three numbers

---
+++ Logic Building/GreaterNumberInThree.java
@@ -0,0 +1,15 @@
+public class GreaterNumberInThree {
+    public static void main(String[] args) {
+        int a = 2,b = 3,c = 4;
+
+        if(a>b){
+            System.out.println("a is greater number");
+        }
+        else if(b>c){
+            System.out.println("b is greater number");
+        }
+        else{
+            System.out.println("c is greater number");
+        }
+    }
+}
