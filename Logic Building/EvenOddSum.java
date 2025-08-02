d975cefb8920733f3bdaada64e91d26f5a0d3a3f
PrinceK93 <princed55x@gmail.com>
2025-08-02 16:01:18 +0530

    Add EvenOddSum, FlipDigit, ReverseNum, and RotateAnum classes with main methods for number manipulation

---
+++ Logic Building/EvenOddSum.java
@@ -0,0 +1,23 @@
+public class EvenOddSum {
+    public static void main(String[] args) {
+        int num = 1234;
+        int position = 0;
+        int evenSum = 0;
+        int oddSum = 0;
+        while (num!=0) {
+        int lastdigit = num % 10;
+        position++;
+
+        if(position % 2 == 0){
+            evenSum = evenSum + lastdigit;
+        }
+        else{
+            oddSum = oddSum +lastdigit;
+        }
+
+        num = num / 10;
+    }
+        System.out.println("odd position sum : " +oddSum);
+        System.out.println("even position sum : " +evenSum);
+    }
+}
---
+++ Logic Building/RotateAnum.java
@@ -0,0 +1,23 @@
+public class RotateAnum {
+
+    public static void main(String[] args) {
+        int num = 32145;
+        int rotation = 2;
+
+        // Count number of digits
+        int digits = (int) Math.log10(num) + 1;
+
+        // Normalize rotation
+        rotation = rotation % digits;
+
+        int divisor = (int) Math.pow(10, rotation);
+        int multiplier = (int) Math.pow(10, digits - rotation);
+
+        int lastPart = num % divisor;       // Get last 2 digits → 45
+        int firstPart = num / divisor;      // Get remaining part → 321
+
+        int rotated = lastPart * multiplier + firstPart;
+
+        System.out.println("Rotated number: " + rotated);
+    }
+}
