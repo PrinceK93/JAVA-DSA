5f2d9be60b7bf5d2383c3ddbd2a7c33705f3d0d9
PrinceK93 <princed55x@gmail.com>
2025-08-03 15:42:23 +0530

    Format DigitsOfNumber class by adding whitespace for improved readability

--- Logic Building/DigitsOfNumber.java
+++ Logic Building/DigitsOfNumber.java
@@ -3,10 +3,12 @@ public class DigitsOfNumber {
         int num = 1234;
         int cpy = num;
         int count = 0;
+
         while(cpy!=0){
             count++;
             cpy =cpy/10;
         }
+        
         while(num!=0){
             int pow =(int)Math.pow(10,count-1);
             System.out.println(num/pow);
