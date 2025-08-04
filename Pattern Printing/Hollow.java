765594d7865476267dd61fc5c115a737a9721770
PrinceK93 <princed55x@gmail.com>
2025-08-04 17:23:06 +0530

    Add Hollow class for hollow square pattern printing

---
+++ Pattern Printing/Hollow.java
@@ -0,0 +1,17 @@
+public class Hollow {
+
+    public static void main(String[] args) {
+        int range = 5;
+        for (int row = 1; row <= range; row++) {
+            for (int col = 1; col <= 5; col++) {
+                if (row == 1 || row == range || col == 1 || col == range) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
