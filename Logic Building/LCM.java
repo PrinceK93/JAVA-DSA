07a533b2afcb89cc49822f84c27d998f4ab2f852
PrinceK93 <princed55x@gmail.com>
2025-08-04 16:24:40 +0530

    Add GCD and LCM classes for greatest common divisor and least common multiple calculations

---
+++ Logic Building/LCM.java
@@ -0,0 +1,16 @@
+public class LCM {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 24;
+        int acpy = a;
+        int bcpy = b;
+
+        while(a%b!=0){
+            int rem = a%b;
+            a = b;
+            b = rem;
+        }
+        int lcm = (acpy*bcpy)/b;
+        System.out.println(lcm);
+    }
+}
