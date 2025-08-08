c272a7907464549da6634d9d45e8d2240a0b641f
PrinceK93 <princed55x@gmail.com>
2025-08-08 23:21:57 +0530

    Add whitespace for improved readability in Hollow and Square classes

--- Pattern Printing/Hollow.java
+++ Pattern Printing/Hollow.java
@@ -2,6 +2,7 @@ public class Hollow {
 
     public static void main(String[] args) {
         int range = 5;
+        
         for (int row = 1; row <= range; row++) {
             for (int col = 1; col <= 5; col++) {
                 if (row == 1 || row == range || col == 1 || col == range) {
--- Pattern Printing/Square.java
+++ Pattern Printing/Square.java
@@ -1,6 +1,7 @@
 public class Square {
     public static void main(String[] args) {
         int range = 5;
+        
         for(int row = 1 ; row <= range ;row++){
             for(int column = 1 ; column <= range ; column++){
                 System.out.print("*");
