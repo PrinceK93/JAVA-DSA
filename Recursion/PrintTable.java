62de925415970ef73a9b97af1e994f5da5b25069
PrinceK93 <princed55x@gmail.com>
2025-08-12 04:27:47 +0530

    Add PrintTable class for printing multiplication tables

---
+++ Recursion/PrintTable.java
@@ -0,0 +1,14 @@
+public class PrintTable {
+
+    static void TablePrint(int num, int i){
+        if(i > 10){
+            return;
+        }
+        System.out.println(num + " x " + i + " = " + (num * i));
+        TablePrint(num, i+1);
+    }
+    public static void main(String[] args) {
+        int num = 5; 
+        TablePrint(num,1);
+    }
+}
