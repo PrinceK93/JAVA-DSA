4076c56f5db1aff1417fffaebfe2bf4061f02805
PrinceK93 <princed55x@gmail.com>
2025-08-16 01:57:40 +0530

    Fix formatting by adding whitespace for better readability in PrintName.java

--- Recursion/PrintName.java
+++ Recursion/PrintName.java
@@ -4,9 +4,9 @@ public class PrintName {
         if(num==0){
             return;
         }
-        
+       
         System.out.println("My Name Is Prince "+ num);
-        NamePrint(num-1);
+         NamePrint(num-1);
     }
 
     public static void main(String[] args) {
