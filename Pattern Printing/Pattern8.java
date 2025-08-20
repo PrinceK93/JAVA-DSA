a652ba6ba0349e3d5702fa18f5b30b3339f693da
PrinceK93 <princed55x@gmail.com>
2025-08-20 10:48:44 +0530

    Remove inline comment for clarity in Pattern8.java

--- Pattern Printing/Pattern8.java
+++ Pattern Printing/Pattern8.java
@@ -1,6 +1,6 @@
 public class Pattern8 {
     public static void main(String[] args) {
-        int n = 5; // height and width
+        int n = 5; 
 
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= n; col++) {
