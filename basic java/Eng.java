0d63ae496bb58a5c157d3fa7a261fddb37dbc332
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-09-28 07:05:08 +0530

    Update Eng.java

--- basic java/Eng.java
+++ basic java/Eng.java
@@ -3,7 +3,7 @@ public class Eng {
     public static void main(String[] args) {
         
         java.lang.String name = "prince";
-        System.out.println(name);
+        System.out.println(name); 
         
     }
 }
