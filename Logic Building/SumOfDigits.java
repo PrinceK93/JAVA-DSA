5898c633b568be276d3c989a8ce8622653c8b0e3
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-10-02 20:41:13 +0530

    Update SumOfDigits.java

--- Logic Building/SumOfDigits.java
+++ Logic Building/SumOfDigits.java
@@ -2,7 +2,7 @@ public class SumOfDigits {
     public static void main(String[] args) {
         int num= 1234;
         int sum = 0;
-        while(num!=0){
+        while(num!=0){ 
         int lastdigit = num % 10;
         sum = sum + lastdigit;
         num = num/10;
