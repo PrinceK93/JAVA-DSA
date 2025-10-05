9be4cbed1b56b7fac0f064767e32a771e3833b8c
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-10-05 22:56:12 +0530

    Update Hollow.java

--- Pattern Printing/Hollow.java
+++ Pattern Printing/Hollow.java
@@ -11,7 +11,7 @@ public class Hollow {
                     System.out.print(" ");
                 }  
             }
-            System.out.println();
+            System.out.println(); 
         }
     }
 }
