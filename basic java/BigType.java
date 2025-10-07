e769b5fd070c71063e2b3c65dba415291d03825d
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-10-07 21:08:25 +0530

    Update BigType.java

--- basic java/BigType.java
+++ basic java/BigType.java
@@ -4,7 +4,7 @@ public class BigType {
     
     public static void main(String[] args) {
         
-        BigInteger b = new BigInteger("191291929192192192219291921");
+        BigInteger b = new BigInteger("191291929192192192219291921"); 
 
         BigInteger b2 = new BigInteger("99999999929192192192219291921"); 
         BigInteger b3 = b.add(b2); 
