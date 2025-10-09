7c101471941f6c04ada1446a78df0fde0b13e781
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-10-09 20:48:20 +0530

    Update printDigit.java

--- Recursion/printDigit.java
+++ Recursion/printDigit.java
@@ -5,7 +5,7 @@ public class printDigit {
         }  
         System.err.println("hello"); 
     }
-            public static void main(String[] args) {
+            public static void main(String[] args) { 
           
     } 
 }
