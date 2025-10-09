c2393073cf5c684cc740b68874fe3fd05ec3982d
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-10-09 20:48:15 +0530

    Update printDigit.java

--- Recursion/printDigit.java
+++ Recursion/printDigit.java
@@ -2,7 +2,7 @@ public class printDigit {
     static void num(int a,int b){
         if(a == 0 || b == 0){
             return; 
-        } 
+        }  
         System.err.println("hello"); 
     }
             public static void main(String[] args) {
