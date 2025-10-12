070d85683eb2b58a8b6192b348421ccf6f957c5e
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-10-13 01:40:22 +0530

    Update PrimeNumber.java

--- Logic Building/PrimeNumber.java
+++ Logic Building/PrimeNumber.java
@@ -7,7 +7,7 @@ public class PrimeNumber {
             isPrime = false;
         } 
 
-        else {
+        else { 
             for (int i = 2; i < num; i++) { 
                 if (num % i == 0) {
                     isPrime = false;
