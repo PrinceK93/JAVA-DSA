c6d0c1528a7cbef73db7f74a91ad48f4479b696f
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-10-14 04:32:13 +0530

    Update DateType.java

--- basic java/DateType.java
+++ basic java/DateType.java
@@ -3,7 +3,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateType {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
     
         Date date = new Date();
         System.out.println(date);
