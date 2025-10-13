a5950d6e7f9d158a76eba61ef1a8ae041ad5ea4a
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-10-13 16:49:06 +0530

    Update Hollow.java

--- Pattern Printing/Hollow.java
+++ Pattern Printing/Hollow.java
@@ -7,9 +7,11 @@ public class Hollow {
             for (int col = 1; col <= 5; col++) { 
                 if (row == 1 || row == range || col == 1 || col == range) { 
                     System.out.print("*");   
-                } else {  
+                } else if {  
                     System.out.print(" ");
                 }  
+                else {  
+                    System.out.print(" ");
             } 
             System.out.println(); 
         }
