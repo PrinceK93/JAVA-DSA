a308a9f4ada93fa69f10f93ad6939938c601d47d
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-11-03 22:21:42 +0530

    Update DateType.java

--- basic java/DateType.java
+++ basic java/DateType.java
@@ -9,7 +9,7 @@ public class DateType {
         System.out.println(date);
    
         // change the date and time and months use Calendar
-        Calendar c = Calendar.getInstance();
+        Calendar c = Calendar.getInstance(); 
         c.setTime(date);
         c.add(Calendar.MONTH,10);
         System.out.println(c.getTime());
