46aeef193fb736d1da6928f7608da765e28e7700
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-11-10 00:38:32 +0530

    Update Pattern7.java

--- Pattern Printing/Pattern7.java
+++ Pattern Printing/Pattern7.java
@@ -5,7 +5,7 @@ public class Pattern7 {
         for (int row = 1; row <= n; row++) { 
             for (int col = 1; col <= n; col++) {
                 if (row == 1 || col == n - row + 1) {   
-                    System.out.print("*");
+                    System.out.print("*"); 
                 } else {  
                     System.out.print(" ");
                 } 
