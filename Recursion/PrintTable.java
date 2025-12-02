37fe33c1d84abc9d0dd5e7981733f464c871ad72
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2025-12-02 20:33:43 +0530

    Update PrintTable.java

--- Recursion/PrintTable.java
+++ Recursion/PrintTable.java
@@ -8,7 +8,7 @@ public class PrintTable {
         TablePrint(num, i+1);
     } 
     public static void main(String[] args) {
-        int num = 2; 
+        int num = 2;  
         TablePrint(num,1); 
     } 
 }
