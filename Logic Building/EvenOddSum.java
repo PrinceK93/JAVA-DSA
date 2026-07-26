086dfb103b4d213154adbf321c115af1d083f321
PrinceK93 <140707024+PrinceK93@users.noreply.github.com>
2026-07-27 00:58:16 +0530

    Fix increment of position variable in EvenOddSum

--- Logic Building/EvenOddSum.java
+++ Logic Building/EvenOddSum.java
@@ -8,7 +8,7 @@ public class EvenOddSum {
          
         while (num!=0) {
         int lastdigit = num % 10; 
-        position++;
+        position++; 
         if(position % 2 == 0){ 
             evenSum = evenSum + lastdigit;
         }
